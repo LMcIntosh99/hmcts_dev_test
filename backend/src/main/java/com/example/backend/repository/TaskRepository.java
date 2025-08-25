7e47abc41b0fb184cc1af876486826aac53ef847
LMcIntosh99 <lsmcintosh99@gmail.com>
2025-08-25 21:30:05 +0100

    Create TaskRepository.java

---
+++ backend/src/main/java/com/example/backend/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+	
+}
